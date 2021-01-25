b9a2ee209fd6f012e3b3fac625676725a636d88e
aaa <aaa@aaa-VirtualBox>
2021-01-25 13:09:22 +0100

    2.0

--- src/tarea1/Tarea1.java
+++ src/tarea1/Tarea1.java
@@ -1,21 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tarea1;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  *
- * @author aaa
+ * @author Gaby Nieva
  */
 public class Tarea1 {
 
     /**
      * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+     * @throws java.io.IOException
+     * */ 
+    public static void main(String[] args) throws IOException {
+       // Declaración de variables
+            String  nombre;
+            int  x,y,z;
+            float a, b, c;
+           
+            BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
+            String entrada;
+            
+            System.out.println("Escribe el valor de x: ");
+            entrada = bufer.readLine();
+            x= Integer.parseInt(entrada);
+            System.out.println("Escribe el valor de y: ");
+            entrada = bufer.readLine();
+            y = Integer.parseInt(entrada);
+            z = y % x;
+            System.out.println("El residuo de dividir y (" + y + ") entre x ( " +x + ") es: " + z);
+            
+            System.out.println("Escribe el valor de a: ");
+            entrada = bufer.readLine();
+            a = Float.parseFloat(entrada);
+            System.out.println("Escribe el valor de b: ");
+            entrada = bufer.readLine();
+            b = Float.parseFloat(entrada);
+            c = a * b ;
+            System.out.println("El resultado de multiplicar a por b es: " + c);           
     
+        System.out.println("Escribe un nombre: ");
+        entrada = bufer.readLine();
+        nombre = entrada;
+        System.out.println("El  nombre que escribiste es: " + nombre);
+    }
 }
